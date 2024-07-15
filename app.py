99180d989481dfa42fc5dcce04c9145fdbf23f45
Pradeeprajamohankumar <114413203+Pradeeprajamohankumar@users.noreply.github.com>
2024-07-15 22:32:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,107 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 from uuid import uuid4
-import hashlib
-import json
-from time import time
-from urllib.parse import urlparse
-import requests
-
-class Blockchain:
-    def __init__(self):
-        self.chain = []
-        self.current_transactions = []
-        self.nodes = set()
-
-        # Create the genesis block
-        self.create_block(previous_hash='1', proof=100)
-
-    def register_node(self, address):
-        parsed_url = urlparse(address)
-        self.nodes.add(parsed_url.netloc)
-
-    def create_block(self, proof, previous_hash):
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
-        }
-        self.current_transactions = []
-        self.chain.append(block)
-        return block
-
-    def new_transaction(self, sender, recipient, amount):
-        self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
-        return self.last_block['index'] + 1
-
-    @staticmethod
-    def hash(block):
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
-
-    @property
-    def last_block(self):
-        return self.chain[-1]
-
-    def proof_of_work(self, last_proof):
-        proof = 0
-        while self.valid_proof(last_proof, proof) is False:
-            proof += 1
-        return proof
-
-    @staticmethod
-    def valid_proof(last_proof, proof):
-        guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
-
-    def valid_chain(self, chain):
-        last_block = chain[0]
-        current_index = 1
-
-        while current_index < len(chain):
-            block = chain[current_index]
-            if block['previous_hash'] != self.hash(last_block):
-                return False
-
-            last_proof = last_block['proof']
-            proof = block['proof']
-            if not self.valid_proof(last_proof, proof):
-                return False
-
-            last_block = block
-            current_index += 1
-
-        return True
-
-    def resolve_conflicts(self):
-        neighbors = self.nodes
-        new_chain = None
-
-        max_length = len(self.chain)
-
-        for node in neighbors:
-            response = requests.get(f'http://{node}/chain')
-
-            if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
-
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
-
-        if new_chain:
-            self.chain = new_chain
-            return True
-        return False
-
-
+from blockchain import Blockchain
 # Instantiate the Flask application
 app = Flask(__name__)
 
@@ -134,6 +33,7 @@ def mine():
     response = {
         'message': "New Block Forged",
         'index': block['index'],
+        'timestamp': block['timestamp'],
         'transactions': block['transactions'],
         'proof': block['proof'],
         'previous_hash': block['previous_hash'],
@@ -159,6 +59,21 @@ def full_chain():
         'chain': blockchain.chain,
         'length': len(blockchain.chain),
     }
+    return jsonify(response), 200
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
     return jsonify(response), 200
 
 if __name__ == '__main__':
